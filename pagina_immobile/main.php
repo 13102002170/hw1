a61fb5ee183d648b25f3cfea2bf49cc27cc92049
stefano_caramagno <105925271+13102002170@users.noreply.github.com>
2024-06-03 02:50:13 +0200

    Delete pagina_immobile directory

--- pagina_immobile/main.php
+++
@@ -1,8 +0,0 @@
-<!-- PHP per MAIN CONTENT -->
-<main>
-
-    <div id="flex-container-main-section1">
-        <!-- Il contenuto viene caricato tramite API REST da main_api.php --> 
-    </div>
-
-</main>
